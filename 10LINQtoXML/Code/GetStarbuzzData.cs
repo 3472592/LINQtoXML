4b58e6cfd6288da272e723e684f8fd15469e959a
3472592 <3472592@gmail.com>
2024-06-27 07:31:52 -0400

    Add files via upload

---
+++ 10LINQtoXML/Code/GetStarbuzzData.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml.Linq;
+
+namespace Code
+{
+    public static class GetStarbuzzData
+    {
+        public static XDocument GetData()
+        {
+            /* You can use an XDocument to create
+            an XML file, and that includes XML
+            files you can read and write using
+            DataContractSerializer. An XMLDocument object
+            represents an XML document. It’s part of the
+            System.Xml.Linq namespace. */
+            XDocument doc = new(
+
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Starbuzz Customer Loyalty Data"),
+
+                new XElement("starbuzzData",
+                    new XAttribute("storeName", "Park Slope"),
+                    new XAttribute("location", "Brooklyn, NY"),
+
+                    new XElement("person",
+                        new XElement("personalInfo",
+                            /* Use XElement objects to create
+                            elements under the XML tree. */
+                            new XElement("name", "Janet Venutian"),
+                            new XElement("zip", 11215)),
+                        new XElement("favoriteDrink", "Choco Macchiato"),
+                        new XElement("moneySpent", 255),
+                        new XElement("visits", 50)),
+
+                    new XElement("person",
+                        new XElement("personalInfo",
+                            new XElement("name", "Liz Nelson"),
+                            new XElement("zip", 11238)),
+                        new XElement("favoriteDrink", "Double Cappuccino"),
+                        new XElement("moneySpent", 150),
+                        new XElement("visits", 35)),
+
+                    new XElement("person",
+                        new XElement("personalInfo",
+                            new XElement("name", "Matt Franks"),
+                            new XElement("zip", 11217)),
+                        new XElement("favoriteDrink", "Zesty Lemon Chai"),
+                        new XElement("moneySpent", 75),
+                        new XElement("visits", 15)),
+
+                    new XElement("person",
+                        new XElement("personalInfo",
+                            new XElement("name", "Joe Ng"),
+                            new XElement("zip", 11217)),
+                        new XElement("favoriteDrink", "Banana Split in a Cup"),
+                        new XElement("moneySpent", 60),
+                        new XElement("visits", 10)),
+
+                    new XElement("person",
+                        new XElement("personalInfo",
+                            new XElement("name", "Sarah Kalter"),
+                            new XElement("zip", 11215)),
+                        new XElement("favoriteDrink", "Boring Coffee"),
+                        new XElement("moneySpent", 110),
+                        new XElement("visits", 15))));
+
+            return doc;
+        }
+    }
+}
---
+++ 10LINQtoXML/Code/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Linq;
+
+namespace Code
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /* The XDocument object’s Load() and
+            Save() methods read and write XML files. 
+            And its ToString() method renders everything
+            inside it as one big XML document. */
+            XDocument doc = GetStarbuzzData.GetData();
+            Console.WriteLine(doc.ToString());
+            doc.Save("starbuzzData.xml");
+            XDocument anotherDoc = XDocument.Load("starbuzzData.xml");
+
+            /* The Descendants() method  returns a reference to an
+            object that you can plug right into LINQ. */
+            var data = from item in doc.Descendants("person")
+                       select new
+                       {
+                           drink = item.Element("favoriteDrink").Value,
+                           moneySpent = item.Element("moneySpent").Value,
+                           /* You already know that LINQ lets you call
+                            methods and use them as part of the query, and
+                            that works really well with the Element() method. 
+                           Element() returns an XElement object, and
+                            you can use its properties to check specific values
+                            in your XML document.*/
+                           zipCode = item.Element("personalInfo").Element("zip").Value
+                       };
+            foreach (var p in data)
+                Console.WriteLine(p.ToString());
+
+            var zipcodeGroups = from item in doc.Descendants("person")
+                                group item.Element("favoriteDrink").Value
+                                by item.Element("personalInfo").Element("zip").Value
+                                into zipcodeGroup
+                                select zipcodeGroup;
+            foreach (var group in zipcodeGroups)
+                Console.WriteLine("{0} favorite drinks in {1}",
+                            group.Distinct().Count(), group.Key);
+
+            XDocument ourBlog = XDocument.Load("http://www.stellman-greene.com/feed");
+            Console.WriteLine(ourBlog.Element("rss").Element("channel").Element("title").Value);
+            var posts = from post in ourBlog.Descendants("item")
+                        select new
+                        {
+                            Title = post.Element("title").Value,
+                            Date = post.Element("pubDate").Value
+                        };
+            foreach (var post in posts)
+                Console.WriteLine(post.ToString());
+        }
+    }
+}
